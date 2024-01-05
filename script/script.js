a32c9f32490a53e71eb0119c89798d27f7f5bb21
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2024-01-05 18:01:33 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -12,3 +12,41 @@ document.addEventListener('click', function (e) {
         hamburger.classList.remove('active')
     }
 })
+
+//LINK SCROLLING
+const aboutUsLink = document.getElementById('about-us-link')
+const aboutUsContent = document.getElementById('about-us')
+
+const servicesLink = document.getElementById('services-link')
+const servicesContent = document.getElementById('services')
+
+const partnersLink = document.getElementById('partners-link')
+const partnersContent = document.getElementById('partners')
+
+const newsLink = document.getElementById('latest-news-link')
+const newsContent = document.getElementById('latest-news')
+
+const contactLink = document.getElementById('contact-us-link')
+const contactContent = document.getElementById('contact-us')
+
+
+aboutUsLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    aboutUsContent.scrollIntoView({behavior:"smooth"})
+})
+servicesLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    servicesContent.scrollIntoView({behavior:"smooth"})
+})
+partnersLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    partnersContent.scrollIntoView({behavior:"smooth"})
+})
+newsLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    newsContent.scrollIntoView({behavior:"smooth"})
+})
+contactLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    contactContent.scrollIntoView({behavior:"smooth"})
+})
